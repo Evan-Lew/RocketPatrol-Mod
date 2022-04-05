392cf1d6cfcf4536488e1a5887cd51690baac70c
Evan-Lew <evanml.lew@gmail.com>
2022-04-05 12:19:57 -0700

    finished the tutorial

--- Rocket Patrol/src/prefabs/Rocket.js
+++ Rocket Patrol/src/prefabs/Rocket.js
@@ -6,6 +6,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);   // add to existing, displayList, updateList
         this.isFiring = false;      // track rocket's firing status
         this.moveSpeed = 2;         // pixels per frame
+        this.sfxRocket = scene.sound.add('sfx_rocket')  // add rocket sfx
     }
 
     update() {
@@ -19,7 +20,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
             }
         }
         // fire button
-        if (Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
+        if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring) {
             this.isFiring = true;
             this.sfxRocket.play();
         }
@@ -31,5 +32,11 @@ class Rocket extends Phaser.GameObjects.Sprite {
         if (this.y <= borderUISize * 3 + borderPadding) {
             this.reset();
         }
+    }
+
+    // reset rocket to "ground"
+    reset() {
+        this.isFiring = false;
+        this.y = game.config.height - borderUISize - borderPadding;
     }
 }

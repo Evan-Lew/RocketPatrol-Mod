62d36719da42883dc262285161e90e8736097bf0
Evan-Lew <evanml.lew@gmail.com>
2022-04-05 12:53:47 -0700

    added new line in Rocket.js

--- Rocket Patrol/src/prefabs/Rocket.js
+++ Rocket Patrol/src/prefabs/Rocket.js
@@ -6,6 +6,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
         scene.add.existing(this);   // add to existing, displayList, updateList
         this.isFiring = false;      // track rocket's firing status
         this.moveSpeed = 2;         // pixels per frame
+        
         this.sfxRocket = scene.sound.add('sfx_rocket')  // add rocket sfx
     }
 
